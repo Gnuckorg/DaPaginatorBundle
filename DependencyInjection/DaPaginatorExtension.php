55a14fdc2716bd78992c7c9f2691728a6edf470b
Gnucki <tprelot@gmail.com>
2014-02-07 14:18:38 +0100

    Pager adapter services

--- DependencyInjection/DaPaginatorExtension.php
+++ DependencyInjection/DaPaginatorExtension.php
@@ -24,6 +24,7 @@ class DaPaginatorExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('pager_adapters.yml');
     }
     
     /**
---
+++ PagerAdapter/AdvancedCallbackAdapter.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Da\PaginatorBundle\PagerAdapter;
+
+use Pagerfanta\Exception\InvalidArgumentException;
+use Pagerfanta\Adapter\AdapterInterface;
+
+/**
+ * @author Thomas Prelot <thomas.prelot@gmail.com>
+ */
+class AdvancedCallbackAdapter implements AdapterInterface
+{
+    private $getNbResultsCallback;
+    private $getNbResultsCallbackParameters;
+    private $getSliceCallback;
+    private $getSliceCallbackParameters;
+
+    /**
+     * @param callable $getNbResultsCallback
+     * @param array $getNbResultsCallbackParameters
+     * @param callable $getSliceCallback
+     * @param array $getSliceCallbackParameters
+     */
+    public function __construct(
+        $getNbResultsCallback,
+        array $getNbResultsCallbackParameters,
+        $getSliceCallback,
+        array $getSliceCallbackParameters
+    )
+    {
+        if (!is_callable($getNbResultsCallback)) {
+            throw new InvalidArgumentException('$getNbResultsCallback should be a callable');
+        }
+        if (!is_callable($getSliceCallback)) {
+            throw new InvalidArgumentException('$getSliceCallback should be a callable');
+        }
+
+        $this->getNbResultsCallback = $getNbResultsCallback;
+        $this->getNbResultsCallbackParameters = $getNbResultsCallbackParameters;
+        $this->getSliceCallback = $getSliceCallback;
+        $this->getSliceCallbackParameters = $getSliceCallbackParameters;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNbResults()
+    {
+        return call_user_func_array($this->getNbResultsCallback, $this->getNbResultsCallbackParameters);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSlice($offset, $length)
+    {
+        $this->getSliceCallbackParameters[] = $offset;
+        $this->getSliceCallbackParameters[] = $length;
+
+        return call_user_func_array($this->getSliceCallback, $this->getSliceCallbackParameters);
+    }
+}
